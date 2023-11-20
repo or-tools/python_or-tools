107433c721643ba777f74c484b64cf7ac9a5e743
Corentin Le Molgat <corentinl@google.com>
2023-11-20 13:56:37 +0100

    python: Fix basic_example.py

--- basic_example.py
+++ basic_example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2010-2022 Google LLC
+# Copyright 2010-2023 Google LLC
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,7 +15,7 @@
 # [START program]
 # [START import]
 from ortools.linear_solver import pywraplp
-from ortools.init import pywrapinit
+from ortools.init.python import init
 # [END import]
 
 
@@ -29,17 +29,17 @@ def main():
 
     # [START variables]
     # Create the variables x and y.
-    x = solver.NumVar(0, 1, 'x')
-    y = solver.NumVar(0, 2, 'y')
+    x_var = solver.NumVar(0, 1, 'x')
+    y_var = solver.NumVar(0, 2, 'y')
 
     print('Number of variables =', solver.NumVariables())
     # [END variables]
 
     # [START constraints]
     # Create a linear constraint, 0 <= x + y <= 2.
-    ct = solver.Constraint(0, 2, 'ct')
-    ct.SetCoefficient(x, 1)
-    ct.SetCoefficient(y, 1)
+    constraint = solver.Constraint(0, 2, 'ct')
+    constraint.SetCoefficient(x_var, 1)
+    constraint.SetCoefficient(y_var, 1)
 
     print('Number of constraints =', solver.NumConstraints())
     # [END constraints]
@@ -47,8 +47,8 @@ def main():
     # [START objective]
     # Create the objective function, 3 * x + y.
     objective = solver.Objective()
-    objective.SetCoefficient(x, 3)
-    objective.SetCoefficient(y, 1)
+    objective.SetCoefficient(x_var, 3)
+    objective.SetCoefficient(y_var, 1)
     objective.SetMaximization()
     # [END objective]
 
@@ -59,17 +59,17 @@ def main():
     # [START print_solution]
     print('Solution:')
     print('Objective value =', objective.Value())
-    print('x =', x.solution_value())
-    print('y =', y.solution_value())
+    print('x =', x_var.solution_value())
+    print('y =', y_var.solution_value())
     # [END print_solution]
 
 
 if __name__ == '__main__':
-    pywrapinit.CppBridge.InitLogging('basic_example.py')
-    cpp_flags = pywrapinit.CppFlags()
-    cpp_flags.logtostderr = True
+    init.CppBridge.init_logging("basic_example.py")
+    cpp_flags = init.CppFlags()
+    cpp_flags.stderrthreshold = True
     cpp_flags.log_prefix = False
-    pywrapinit.CppBridge.SetFlags(cpp_flags)
+    init.CppBridge.set_flags(cpp_flags)
 
     main()
 # [END program]
